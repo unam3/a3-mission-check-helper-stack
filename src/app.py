8513b7d554921e734e8d4857f244356926b77094
Flowers Safety <40meta@gmail.com>
2018-12-18 00:26:52 +0600

    check the fact that file was actually uploaded

--- src/app.py
+++ src/app.py
@@ -1,24 +1,19 @@
 from flask import Flask, request, render_template#, url_for
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 #url_for('static', filename='style.css')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
-    print request, request.mimetype, request.data, request.headers
+    print request, request.mimetype, len(request.files)#, request.headers
 
-    if request.method == 'POST':
+    # add file length check
+    if request.method == 'POST' and len(request.files) == 1 and request.files.has_key('mission_file'):
 
-        return 'POST processing'
-
-        #return process_file()
+        return render_template('report.html', data='213')
 
     else:
-
-        #return show_the_login_form()
-
-        #return 'Hello, World!'
 
         return render_template('upload.html')
