afff680b61a4b98c0b47415f1a2713f872581a13
Flowers Safety <40meta@gmail.com>
2020-02-13 23:48:43 +0600

    project compiling script was refactored

--- compile_repos.sh
+++ compile_repos.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-destination=~/checker_new_version
-current_dir=$(pwd)
+function compile_repos_to_folder {
+    destination=$1
+    current_dir=$(pwd)
 
-rm -rf $destination $destination.7z
+    # TODO: add check if dir/file already exists
 
-# TODO: add check if dir/file already exists
+    git archive --format=tar HEAD | (cd $destination && tar xf -) &&
+    #https://stackoverflow.com/questions/52955067/how-to-access-variables-used-in-git-submodule-foreach-from-outside
+    # only $sm_path is set
+    #git submodule foreach --recursive 'echo qwe $destination/$sm_path; ls $destination/$sm_path;'
+    # only $destination is set
+    #git submodule foreach --recursive "echo qwe $destination/$sm_path; ls $destination/$sm_path;" &&
+    echo $destination > TEMP_DESTINATION &&
+    #git submodule foreach --recursive 'pwd; echo $(cat $toplevel/TEMP_DESTINATION)/' &&
+    git submodule foreach 'git archive --format=tar $sha1 | (cd $(cat $toplevel/TEMP_DESTINATION)/$sm_path && tar xf -);' &&
+    rm TEMP_DESTINATION &&
+    # one can use $(git submodule satus) parsing for path to submodule
+    echo "const localizations = {$(cat $destination/src/check/msg_ru) $(cat $destination/src/check/msg_en) };" > $destination/src/templates/localizations &&
 
-cd ~/a3/ &&
-git archive --format=tar HEAD | (mkdir -p $destination/src/check && cd $destination/src/check && tar xf -) &&
-cd ~/mission_checker/ &&
-git archive --format=tar HEAD | (cd $destination/ && tar xf -) &&
-echo "const localizations = {$(cat $destination/src/check/msg_ru) $(cat $destination/src/check/msg_en) };" > $destination/src/templates/localizations &&
+    cd $current_dir &&
 
-7zr a $destination.7z $destination &&
+    echo "New checker version successfully compiled into $destination"
+}
 
-rm -rf $destination &&
-
-cd $current_dir &&
-
-echo "New checker version successfully compiled to $destination.7z"
+if [[ -z $1 ]];
+    then echo "first parameter must be absolute path to deploy destination folder";
+    else compile_repos_to_folder $1;
+fi
